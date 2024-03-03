34f2de3fc4690e4971af59be545be0db90cc2a95
Geordi Van Nieuland <vannieuland.geordi@gmail.com>
2024-03-03 08:52:54 +0100

    env changed

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,12 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
 };
+
+console.log(process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID);
+console.log(process.env);
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const firestoreDB = getFirestore(firebaseApp);
